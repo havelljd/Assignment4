6238e50313872a13a25cb799e5115a4a71e8b0f3
Jeffrey Havell <havelljd@gmail.com>
2021-02-10 21:03:07 -0700

    Added a few comments

--- Models/Restaurants.cs
+++ Models/Restaurants.cs
@@ -17,7 +17,7 @@ namespace Restaurant4.Models
         [Required]
         [Display(Name = "Phone")]
         [DataType(DataType.PhoneNumber)]
-        [RegularExpression(@"^(1-)?\d{3}-\d{3}-\d{4}$", ErrorMessage = "Not a valid phone number")]
+        [RegularExpression(@"^(1-)?\d{3}-\d{3}-\d{4}$", ErrorMessage = "Not a valid phone number")] /*This regex makes sure its the right amount of digits and has the right dashes*/
         public string PhoneNumber { get; set; }
 
     }
